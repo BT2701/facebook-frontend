ea18f5dd12d7c56e9a45fb0d9c5a70413ec1a72a
thientranreal <thientt2612@gmail.com>
2024-09-24 21:50:41 +0700

    add chat box

--- src/components/navbar/CenterLinks.jsx
+++ src/components/navbar/CenterLinks.jsx
@@ -5,24 +5,29 @@ import { AiOutlineUserAdd } from "react-icons/ai";
 import { Icon } from "@chakra-ui/icons";
 import { Link } from "react-router-dom";
 
-const CenterButton = ({path, iconName }) => {
-    return (
-        <Center mx={1}>
-            <Link to={path}>
-                <Button colorScheme={'whiteAlpha'} w={'110px'} h={'50px'} _hover={{ bg: '#f0f2f5' }}><Icon as={iconName} w={7} h={7} color={'grey'} /></Button>
-            </Link>
-        </Center>
-    );
+const CenterButton = ({ path, iconName }) => {
+  return (
+    <Center mx={1}>
+      <Link to={path}>
+        <Button
+          colorScheme={"whiteAlpha"}
+          w={"110px"}
+          h={"50px"}
+          _hover={{ bg: "#f0f2f5" }}
+        >
+          <Icon as={iconName} w={7} h={7} color={"grey"} />
+        </Button>
+      </Link>
+    </Center>
+  );
 };
 
-
 export const CenterLinks = () => {
-
-    return (
-        <>
-            <CenterButton path={'/'} iconName={VscHome} />
-            <CenterButton path={'/groups'} iconName={MdGroups} />
-            <CenterButton path={'/friends'} iconName={AiOutlineUserAdd} />
-        </>
-    );
+  return (
+    <>
+      <CenterButton path={"/"} iconName={VscHome} />
+      <CenterButton path={"/groups"} iconName={MdGroups} />
+      <CenterButton path={"/friends"} iconName={AiOutlineUserAdd} />
+    </>
+  );
 };
