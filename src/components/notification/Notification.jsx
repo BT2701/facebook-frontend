fa738483be9ad5cff75b5fc6dabd4749829899fb
Duong Thanh Truong <truongnopro1111@gmail.com>
2024-10-06 23:24:43 +0700

    handled: markAsRead, markAllAsRead

--- src/components/notification/Notification.jsx
+++ src/components/notification/Notification.jsx
@@ -10,6 +10,8 @@ import {
     MenuList,
     Text,
     VStack,
+    Button,
+    Flex
 } from "@chakra-ui/react";
 import { BellIcon } from "@chakra-ui/icons";
 import './Notification.css'
@@ -17,14 +19,15 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import formatTimeFromDatabase from "../sharedComponents/formatTimeFromDatabase";
 
-const NotificationItem = ({ avatarSrc, title, message, time }) => (
-    
+const NotificationItem = ({ avatarSrc, title, message, time, is_read, onClick }) => (
     <MenuItem
         _hover={{ background: "#f0f2f5" }}
         p={3}
         borderRadius="md"
         alignItems="center"
         className="notification-item"
+        bg={is_read === 0 ? "gray.100" : "white"}
+        onClick={onClick}
     >
         <Avatar src={avatarSrc} size="mm" mr={3} className="notification-item-avt" />
         <Box className="notification-item-box">
@@ -45,23 +48,41 @@ const NotificationItem = ({ avatarSrc, title, message, time }) => (
 );
 
 const Notifications = () => {
-    const [notificationList, setNotificationList]= useState([]);
-    const [currentUser, setCurrentUser]= useState(5);           //dat tam gia tri, doi co du lieu tu user service
+    const [notificationList, setNotificationList] = useState([]);
+    const [currentUser, setCurrentUser] = useState(5); //dat tam gia tri, doi co du lieu tu user service
+    const [readNotification, setReadNotification]= useState(0);
 
     useEffect(() => {
         const fetchData = async () => {
-          try {
-    
-            const response = await axios.get(`${process.env.REACT_APP_API_URL}/notification/receiver/`+currentUser);
-            setNotificationList(response.data);
-    
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
+            try {
+                const response = await axios.get(`${process.env.REACT_APP_API_URL}/notification/receiver/` + currentUser);
+                setNotificationList(response.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
         };
-    
+
         fetchData();
-      }, []);
+        // setReadNotification(0);
+    }, [readNotification]);
+
+    const markAllAsRead = async () => {
+        try {
+            await axios.put(`${process.env.REACT_APP_API_URL}/notification/markAllAsRead/` + currentUser);
+            setReadNotification(1);
+        } catch (error) {
+            console.error('Error marking all as read:', error);
+        }
+    };
+    const markAsRead = async (id) => {
+        try {
+            await axios.put(`${process.env.REACT_APP_API_URL}/notification/${id}`);
+            setReadNotification(1);
+        } catch (error) {
+            console.error('Error marking notification as read:', error);
+        }
+    };
+
     return (
         <Center mr={4}>
             <Menu>
@@ -72,44 +93,45 @@ const Notifications = () => {
                     rounded="full"
                     position="relative"
                 />
-                <MenuList w="360px" maxH="400px" overflowY="auto" p={2} boxShadow="lg">
-                    <VStack align="stretch">
-                    {notificationList.length === 0 ? (
-                <p>Empty</p>
-              ) : (
-                notificationList.map((notification) => (
-                    <NotificationItem
-                            avatarSrc="https://via.placeholder.com/50"
-                            title={notification.user}
-                            message={notification.content}
-                            time={formatTimeFromDatabase(notification.timeline)}
-                        />
-                ))
-            )}
-                        
-                        {/* <NotificationItem
-                            avatarSrc="https://via.placeholder.com/50"
-                            title="Van Du"
-                            message="commented on your photo"
-                            time="10 mins ago"
-                        />
-                        <NotificationItem
-                            avatarSrc="https://via.placeholder.com/50"
-                            title="Tan Dat"
-                            message="sent you a friend request"
-                            time="1 hour ago"
-                        />
-                        <NotificationItem
-                            avatarSrc="https://via.placeholder.com/50"
-                            title="Trung Thien"
-                            message="accept your friend request"
-                            time="1 hour ago"
-                        /> */}
-                    </VStack>
+                <MenuList w="360px" maxH="400px" p={0} boxShadow="lg">
+                    {/* Tiêu đề và nút "Đánh dấu đã đọc" */}
+                    <Box p={3} borderBottom="1px solid #e2e8f0">
+                        <Flex justifyContent="space-between" alignItems="center" height={8}>
+                            <Text fontSize="lg" fontWeight="bold">
+                                Thông báo
+                            </Text>
+                            {notificationList.length > 0 && (
+                                <Button className="notification-read-all" size="sm" colorScheme="blue" onClick={markAllAsRead}>
+                                    Đánh dấu đã đọc
+                                </Button>
+                            )}
+                        </Flex>
+                    </Box>
+                    {/* Danh sách thông báo */}
+                    <Box maxH="320px" overflowY="auto" p={2}>
+                        <VStack align="stretch">
+                            {notificationList.length === 0 ? (
+                                <Text>Empty</Text>
+                            ) : (
+                                notificationList.map((notification) => (
+                                    <NotificationItem
+                                        key={notification.id}
+                                        avatarSrc="https://via.placeholder.com/50"
+                                        title={notification.user}
+                                        message={notification.content}
+                                        time={formatTimeFromDatabase(notification.timeline)}
+                                        is_read={notification.is_read}
+                                        onClick={() => markAsRead(notification.id)}
+                                    />
+                                ))
+                            )}
+                        </VStack>
+                    </Box>
                 </MenuList>
             </Menu>
         </Center>
     );
 };
+
 
 export default Notifications;
