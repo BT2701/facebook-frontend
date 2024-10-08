fe1949b39ddba5cfb7d9c85113927ae48b1bfa6d
thientranreal <thientt2612@gmail.com>
2024-10-08 14:37:20 +0700

    create msg

--- src/components/navbar/ChatBox.jsx
+++ src/components/navbar/ChatBox.jsx
@@ -12,6 +12,7 @@ import { FiSend, FiSmile, FiPaperclip } from "react-icons/fi";
 import ChatMessage from "./ChatMessage";
 import { useEffect, useState } from "react";
 import { getMessagesByUserIdAndContactId } from "../../utils/getData";
+import axios from "axios";
 
 export default function ChatBox({
   isOpen,
@@ -22,11 +23,66 @@ export default function ChatBox({
   contactName,
   status,
 }) {
+  const currentUser = 1;
+
+  // State for all messages
   const [messages, setMessages] = useState([]);
 
-  const getMessages = async () => {
-    const currentUser = 1;
+  // State for message input
+  const [msgInput, setMsgInput] = useState("");
 
+  // Add message to db
+  const postMsg = async (msg) => {
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_URL}/message`,
+        msg,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      return response;
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  // Handle send message
+  const handleSendMsg = async () => {
+    if (msgInput) {
+      try {
+        // Add message to database
+        const response = await postMsg({
+          Sender: currentUser,
+          Receiver: contactId,
+          CreatedAt: new Date().toISOString().split(".")[0],
+          Content: msgInput,
+        });
+
+        if (response && response.data && response.data.id) {
+          // Update messages
+          setMessages((prev) => [
+            ...prev,
+            {
+              id: response.data.id,
+              message: { text: msgInput },
+              isFromYou: true,
+            },
+          ]);
+
+          setMsgInput("");
+        }
+      } catch (error) {
+        console.error("Error sending message:", error);
+      }
+    }
+  };
+
+  // Get messages from server
+  const getMessages = async () => {
     const response = await getMessagesByUserIdAndContactId(
       currentUser,
       contactId
@@ -145,6 +201,8 @@ export default function ChatBox({
         <Input
           variant="unstyled"
           placeholder="Type a message..."
+          value={msgInput}
+          onChange={(e) => setMsgInput(e.target.value)}
           size="md"
           borderRadius="none"
           flex={1}
@@ -157,6 +215,7 @@ export default function ChatBox({
           colorScheme="blue"
           variant="ghost"
           aria-label="Send message"
+          onClick={handleSendMsg}
         />
       </Box>
       {/* End Chat Input */}
