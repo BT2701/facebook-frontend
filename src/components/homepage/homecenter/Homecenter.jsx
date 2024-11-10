c1434c742547dbbd23e73fb6259999339670052d
PhamVanDu <phamvandu1m78@gmail.com>
2024-11-10 15:48:18 +0700

    update display infor user create post and infor user comment

--- src/components/homepage/homecenter/Homecenter.jsx
+++ src/components/homepage/homecenter/Homecenter.jsx
@@ -6,29 +6,83 @@ import { MessageSender } from "./MessageSender";
 import { Feed } from "./Feed";
 import { Box } from "@chakra-ui/react";
 import formatTimeFromDatabase from "../../sharedComponents/formatTimeFromDatabase";
+import { useUser } from "../../../context/UserContext";
+import { getUserById } from "../../../utils/getData";
+import { addDays } from "date-fns";
 
 export const Homecenter = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
-  const currentUserId = 1;
+  const { currentUser } = useUser(); // id current user 
   const postsPerPage = 3; // Number of posts to fetch at once
   const [lastPostId, setLastPostId] = useState(null);
+  const updatePostInfor = async (userId, post) => {
+    try {
+      const response = await getUserById(userId);
+      if (response && response.data) {
+        post.profilePic = response.data.avt;
+        post.profileName = response.data.name;
+      } else {
+        console.error('Error: response or response.data is undefined');
+        // // Handle the case where response or response.data is missing
+        // post.profilePic = 'https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp'; // Or some default value
+        // post.profileName = 'Anonymous'; // Or some default name
+      }
+      if (post.comments.$values.length && Array.isArray(post.comments.$values)) {
+        const updatedComments = await Promise.all(
+          post.comments.$values.map(async (comment) => {
+            return await updateCommentInfor(comment.userId, comment);
+          })
+        );
+        post.comments.$value = updatedComments;
+      }
+      return post;
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+      // // Handle the error (e.g., return default values or an empty object)
+      // post.profilePic = "https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp';
+      // post.profileName = 'Anonymous';
+      return post;
+    }
+  };
+
+  const updateCommentInfor = async (userId, comment) => {
+    try {
+      const response = await getUserById(userId);
+      if (response && response.data) {
+        comment.profilePic = response.data.avt;
+        comment.profileName = response.data.name;
+      } else {
+        console.error('Error: response or response.data is undefined');
+        // // Handle the case where response or response.data is missing
+        // post.profilePic = 'https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp'; // Or some default value
+        // post.profileName = 'Anonymous'; // Or some default name
+      }
+
+      return comment;
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+      // // Handle the error (e.g., return default values or an empty object)
+      // post.profilePic = "https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp';
+      // post.profileName = 'Anonymous';
+      return comment;
+    }
+  };
+
   const fetchPosts = async () => {
     setLoading(true);
     try {
       // Construct the URL based on lastId
       const url = lastPostId
-        ? `http://localhost:8001/api/post/${currentUserId}?lastPostId=${lastPostId}&limit=${postsPerPage}`
-        : `http://localhost:8001/api/post/${currentUserId}?limit=${postsPerPage}`;
+        ? `http://localhost:8001/api/post/${currentUser}?lastPostId=${lastPostId}&limit=${postsPerPage}`
+        : `http://localhost:8001/api/post/${currentUser}?limit=${postsPerPage}`;
 
       const response = await axios.get(url);
       // Check if $values is an array and not null
       if (response.data.$values && Array.isArray(response.data.$values)) {
         const fetchedPosts = response.data.$values;
-
         if (fetchedPosts.length > 0) {
-          // Set the lastId to the last fetched post's id
-
+          fetchedPosts.map((post) => updatePostInfor(post.userId, post));
           setLastPostId(fetchedPosts[fetchedPosts.length - 1].id);
           setPosts((prevPosts) => [...prevPosts, ...fetchedPosts]);
         } else {
@@ -48,8 +102,7 @@ export const Homecenter = () => {
   // Initial fetch of posts
   useEffect(() => {
     fetchPosts();
-    // alert("hello");
-  }, [lastPostId]); // Only run once on mount
+  }, [lastPostId]);
 
   // Update comments for a specific post
   const updateCommentsForPost = (postId, updatedComments) => {
@@ -58,7 +111,8 @@ export const Homecenter = () => {
         post.id === postId ? { ...post, comments: { $values: updatedComments } } : post
       )
     );
-  };  // Handle scrolling to load more posts
+  };
+  // Handle scrolling to load more posts
   const handleScroll = useCallback(() => {
     const bottom = window.innerHeight + window.scrollY >= document.body.offsetHeight - 100;
     if (bottom && !loading) {
@@ -66,7 +120,6 @@ export const Homecenter = () => {
       fetchPosts();
     }
   }, [loading]);
-
   // // Add event listener for scrolling
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
@@ -79,7 +132,7 @@ export const Homecenter = () => {
     <div className="Homecenter">
       <StoryReel />
       <Box mb={"7px"} w={"143%"}>
-        <MessageSender wid={"100%"} setPosts={setPosts} currentUserId={currentUserId} fetchPosts={fetchPosts} setLastPostId={setLastPostId} />
+        <MessageSender wid={"100%"} setPosts={setPosts} currentUserId={currentUser} setLastPostId={setLastPostId} updatePostInfor={updatePostInfor} />
       </Box>
 
 
@@ -88,21 +141,21 @@ export const Homecenter = () => {
           <div key={post.id}>
             <Feed
               postId={post.id}
-              profilePic={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_FWF2judaujT30K9sMf-tZFhMWpgP6xCemw&s"}
+              profilePic={post.profilePic}
               content={post.content}
               timeStamp={formatTimeFromDatabase(post.timeline)}
-              userName={post.userId}
+              userName={post.profileName}
               postImage={post.image}
               likedByCurrentUser={post.likedByCurrentUser}
               likeCount={post.reactions.$values.length}
               commentList={post.comments.$values}
-              currentUserId={currentUserId}
+              currentUserId={currentUser}
               userCreatePost={post.userId}
               setPosts={setPosts}
               posts={posts}
               updateComments={updateCommentsForPost}
-              fetchPosts={fetchPosts}
-            // setLastPostId={setLastPostId} // Pass setLastId if needed for other logic
+              updatePostInfor={updatePostInfor}
+              updateCommentInfor={updateCommentInfor}
             />
           </div>
         ))
